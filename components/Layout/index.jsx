5f0ab53a58551ea1283a08f447488ac29144ec29
daleinen7 <daleinen@gmail.com>
2021-04-13 19:58:36 -0500

    auth connected with google

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -20,14 +20,14 @@ export default function Layout({ children }) {
       <header>
         <h1>Col-Echo</h1>
 
-      {!session && <>
-        Not signed in <br/>
-        <button onClick={() => signIn()}>Sign in</button>
-      </>}
-      {session && <>
-        Signed in as {session.user.email} <br/>
-        <button onClick={() => signOut()}>Sign out</button>
-      </>}
+        {!session && <>
+          Not signed in <br/>
+          <button onClick={() => signIn()}>Sign in</button>
+        </>}
+        {session && <>
+          Signed in as {session.user.email} <br/>
+          <button onClick={() => signOut()}>Sign out</button>
+        </>}
 
       </header>
       <main>{children}</main>
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,23 +6,16 @@ export default NextAuth({
   providers: [
     Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      clientSecret: process.env.GOOGLE_SECRET
     }),
     Providers.Facebook({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET
     }),
     Providers.Email({
-      server: {
-        host:"",
-        port:"",
-        auth: {
-          user: "",
-          pass: ""
-        },
-        from: "",
-      }
-    })
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
     // ...add more providers here
   ],
 
